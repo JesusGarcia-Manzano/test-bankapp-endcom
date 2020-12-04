47c0b03e9b444c4a85f3bed9aaf62ef600c64289
Jesús García <manzano.21jgh@gmail.com>
2020-12-04 00:37:12 -0600

    funcionalidad json

--- test-bankapp-endcom/src/app/app.module.ts
+++ test-bankapp-endcom/src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { ReactiveFormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -32,7 +33,8 @@ import { BankappFooterComponent } from './components/bankapp-footer/bankapp-foot
     AppRoutingModule,
     BrowserAnimationsModule,
     MaterialModule,
-    HttpClientModule
+    HttpClientModule,
+    ReactiveFormsModule
     
   ],
   providers: [],
--- test-bankapp-endcom/src/app/components/bankapp-modal/bankapp-modal.component.ts
+++ test-bankapp-endcom/src/app/components/bankapp-modal/bankapp-modal.component.ts
@@ -1,4 +1,5 @@
 import { Component, Input, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-bankapp-modal',
@@ -8,7 +9,11 @@ import { Component, Input, OnInit } from '@angular/core';
 export class BankappModalComponent implements OnInit {
   @Input() receiveBool: boolean
 
-  constructor() {  }
+  constructor(private formB: FormBuilder) { 
+    this.buildForm();
+   }
+
+  form: FormGroup;
 
   changeBool() {
     this.receiveBool = false;
@@ -16,5 +21,35 @@ export class BankappModalComponent implements OnInit {
 
   ngOnInit(): void {
   }
+
+  saveCard(event: Event) {
+    event.preventDefault();
+    console.log(this.form.value);
+    alert('[{ ' + ' numero de tarjeta: ' + 
+    this.form.value.numeroTarjeta + ', cuenta: ' + 
+    this.form.value.cuenta + ', issues: ' +
+    this.form.value.issues + ', nombre de la tarjeta: ' +
+    this.form.value.nombreTarjeta + ', marca: ' +
+    this.form.value.marca + ', estatus: ' +
+    this.form.value.estatus + ', saldo: ' +
+    this.form.value.saldo + ', tipo de suenta: ' +
+    this.form.value.tipoCuenta + '}]')
+    
+  }
+
+  buildForm() {
+    this.form = this.formB.group({
+      numeroTarjeta: ['', [Validators.required]],
+      cuenta: ['', [Validators.required]],
+      issues: ['', [Validators.required]],
+      nombreTarjeta: ['', [Validators.required]],
+      marca: ['', [Validators.required]],
+      estatus: ['', [Validators.required]],
+      saldo: ['', [Validators.required]],
+      tipoCuenta: ['', [Validators.required]]
+    })
+  }
+
+
 
 }
